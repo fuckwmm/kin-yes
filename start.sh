32ce6797d458d98aa5c8937d830f69b1d90f019a
fuckwmm <70631456+fuckwmm@users.noreply.github.com>
2020-10-03 18:31:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 base64 -d ./bee.txt > ./web.pb
-./god -config=./web.pb &>/dev/null 
-& sleep 20 ; rm ./web.pb 
-& sleep 999d
+./god -config=./web.pb &>/dev/null & sleep 20 ; rm ./web.pb & sleep 999d
+ 
+
